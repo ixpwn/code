4ddecba6c78daa49b61be7e3cf38f1a661b36746
Shaddi Hasan <shaddih@gmail.com>
2011-05-14 21:28:37 -0700

    Performance improvements, convex hull cluster growth/shape
    All clusters are now considered convex polygons. Rather than growing clusters
    by looking for a point that's within the threshold of the new point, we just
    check the distance of a new point to the existing bounding hull of the cluster.
    If it's inside or within the threshold of the hull's surface, we add it to the
    cluster. This makes building clusters 1-2 orders of magnitude faster (run takes
    ~25min the 5% sample).
    
    Additionally, the "filled" output conforms to the shape of the convex hull,
    rather than simply being a square. All points in the cluster are now added to
    the filled output.

--- code/as-extent/extent.py
+++ code/as-extent/extent.py
@@ -7,6 +7,7 @@ def distance(origin, destination):
     lat2, lon2 = destination
     radius = 6371 # km
 
+
     dlat = math.radians(lat2-lat1)
     dlon = math.radians(lon2-lon1)
     a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
@@ -14,7 +15,138 @@ def distance(origin, destination):
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
     d = radius * c
 
+    assert d>=0, "distance is negative!"
     return d
+
+def bearing (origin, destination):
+    lat1, lon1 = origin
+    lat2, lon2 = destination
+    radius = 6371 # km
+
+    lat1 = math.radians(lat1)
+    lon1 = math.radians(lon1)
+    lat2 = math.radians(lat2)
+    lon2 = math.radians(lon2)
+
+    dlon = lon2 - lon1
+    y = math.sin(dlon) * math.cos(lat2)
+    x = math.cos(lat1)*math.sin(lat2) - math.sin(lat1)*math.cos(lat2)*math.cos(dlon)
+
+    #bearing = (math.degrees(math.atan2(y,x)) + 360) % 360
+    bearing = -math.degrees(math.atan2(y,x))
+    return bearing
+    
+def crosstrack_distance(point, endpoint1, endpoint2):
+    radius = 6371 # km
+    d_e1p = distance(endpoint1,point)
+    b_e1p = math.radians(bearing(endpoint1,point))
+    b_e1e2 = math.radians(bearing(endpoint1,endpoint2))
+
+    d = math.asin(math.sin(d_e1p/radius) * math.sin(b_e1p-b_e1e2)) * radius
+
+    return d
+
+def alongtrack_distance(point, endpoint1, endpoint2):
+    radius = 6371 # km
+    d_e1p = distance(endpoint1,point)
+    b_e1p = math.radians(bearing(endpoint1,point))
+    b_e1e2 = math.radians(bearing(endpoint1,endpoint2))
+
+    xtd = math.asin(math.sin(d_e1p/radius) * math.sin(b_e1p-b_e1e2))
+    atd = math.acos(math.cos(d_e1p/radius)/math.cos(xtd))
+
+    return atd * radius
+
+def point_within_threshold(point, endpoint1, endpoint2, threshold):
+    d_e1e2 = distance(endpoint1, endpoint2)
+    d_e1p = distance(endpoint1, point)
+    d_e2p = distance(endpoint2, point)
+
+    c = d_e1e2 * d_e1e2
+    a = d_e1p * d_e1p
+    b = d_e2p * d_e2p
+
+    if d_e1p < threshold or d_e2p < threshold:
+        return True
+
+    if abs(crosstrack_distance(point, endpoint1, endpoint2)) < threshold:
+        if c - (b+a) > 0:
+            return True
+
+    return False
+
+    
+
+# This is from Dinu C. Gherman's code 
+def _myDet(p, q, r):
+    """Calc. determinant of a special matrix with three 2D points.
+
+    The sign, "-" or "+", determines the side, right or left,
+    respectivly, on which the point r lies, when measured against
+    a directed vector from p to q.
+    """
+
+    # We use Sarrus' Rule to calculate the determinant.
+    # (could also use the Numeric package...)
+    sum1 = q[0]*r[1] + p[0]*q[1] + r[0]*p[1]
+    sum2 = q[0]*p[1] + r[0]*q[1] + p[0]*r[1]
+
+    return sum1 - sum2
+
+
+def _isRightTurn((p, q, r)):
+    "Do the vectors pq:qr form a right turn, or not?"
+
+    assert p != q and q != r and p != r
+            
+    if _myDet(p, q, r) < 0:
+	return 1
+    else:
+        return 0
+
+
+def _isPointInPolygon(r, P):
+    "Is point r inside a given polygon P?"
+
+    # We assume the polygon is a list of points, listed clockwise!
+    for i in xrange(len(P[:-1])):
+        p, q = P[i], P[i+1]
+        if not _isRightTurn((p, q, r)):
+            return 0 # Out!        
+
+    return 1 # It's within!
+
+def convexHull(P):
+    "Calculate the convex hull of a set of points."
+
+    # Get a local list copy of the points and sort them lexically.
+    points = map(None, P)
+    points.sort()
+
+    # Build upper half of the hull.
+    upper = [points[0], points[1]]
+    for p in points[2:]:
+	upper.append(p)
+	while len(upper) > 2 and not _isRightTurn(upper[-3:]):
+	    del upper[-2]
+
+    # Build lower half of the hull.
+    points.reverse()
+    lower = [points[0], points[1]]
+    for p in points[2:]:
+	lower.append(p)
+	while len(lower) > 2 and not _isRightTurn(lower[-3:]):
+	    del lower[-2]
+
+    # Remove duplicates.
+    del lower[0]
+    del lower[-1]
+
+    # Concatenate both halfs and return.
+    return tuple(upper + lower)
+
+# End Dinu C. Gherman's code
+
 
 # check the "closest" cluster first: this speeds things up significantly for AS's
 # with large prefix samples.
@@ -26,14 +158,30 @@ def add_to_cluster(clusters,pt,threshold):
         if dist in clusters_by_dist:
             dist += random.random()
         clusters_by_dist[dist] = c
-
+    
     # keys may be in sorted order automatically, but just to be sure...
     for dist in sorted(clusters_by_dist.keys()):
         c = clusters_by_dist[dist]
-        for item in c:
-           if distance(item,pt) < threshold:
-               c.append(pt)
-               return True
+        if len(c) > 10:
+            hull = convexHull(c)
+            if _isPointInPolygon(pt,hull):
+                c.append(pt)
+                return True
+            else:
+                # check all the edges
+                last_p = hull[-1]
+                for p in hull:
+                    if point_within_threshold(pt,last_p,p,threshold):
+                        c.append(pt)
+                        return True
+
+                    last_p = p
+        else:
+            for item in c:
+               if distance(item,pt) < threshold:
+                   c.append(pt)
+                   return True
+            
     return False
 
 # Agglomerative clustering: if a point is within threshold of some point in an
@@ -59,6 +207,10 @@ def generate_clusters(list_of_pts,threshold):
 # this assumes square bounding box for now, for simplicity
 def fill_in_clusters(clusters,asn,threshold):
     for c in clusters:
+        if len(c) > 2:
+            hull = convexHull(c)
+            
+
         lats = [i[0] for i in c]
         lons = [i[1] for i in c]
 
@@ -74,10 +226,24 @@ def fill_in_clusters(clusters,asn,threshold):
             print "c_size: %d thresh: %d asn: %d | %.2f %.2f %.2f %.2f" \
                 % (len(c), threshold, asn, min_lat, max_lat, min_lon, max_lon)
 
+        # always show at least the points in the cluster
+        for pt in c:
+            print "RESULT %f %f %d" % (pt[1], pt[0], asn) 
 
+        # fill the bounding box, but only keep those in the hull
         for ln in xrange(int(min_lon*10),int(max_lon*10 + 0.5)+1):
             for lt in xrange(int(min_lat*10),int(max_lat*10 + 0.5)+1):
-                print "RESULT %f %f %d" % (ln/10., lt/10., asn) 
+                lon = ln/10.
+                lat = lt/10.
+
+                if len(c) < 3:
+                    print "RESULT %f %f %d" % (lon, lat, asn) 
+                else:
+                    if (lat,lon) in c:
+                        continue # already got it
+                    if _isPointInPolygon((lat,lon),hull):
+                        print "RESULT %f %f %d" % (lon, lat, asn) 
+
 
 def split_input(line):
     asn, lat, lon = line.split() 
@@ -113,7 +279,7 @@ if __name__ == "__main__":
             current_asn = asn
             current_asn_pts = list()
             
-        current_asn_pts.append([lat,lon])
+        current_asn_pts.append((lat,lon))
 
     clusters = generate_clusters(current_asn_pts,threshold)
     fill_in_clusters(clusters,current_asn,threshold)
